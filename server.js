65c0545bf4cc3cbe3d41172fd6b241dd37d7b88c
Mafindi <umarabdulfatah2023@gmail.com>
2026-03-03 01:21:50 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({
+    secret: 'schoolportal',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Login route
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === 'admin' && password === '1234') {
+        req.session.user = 'admin';
+        return res.redirect('/admin.html');
+    }
+
+    if (username === 'student' && password === '1234') {
+        req.session.user = 'student';
+        return res.redirect('/student.html');
+    }
+
+    res.send("Invalid login credentials");
+});
+
+// Protect admin page
+app.get('/admin.html', (req, res, next) => {
+    if (req.session.user === 'admin') next();
+    else res.redirect('/login.html');
+});
+
+// Protect student page
+app.get('/student.html', (req, res, next) => {
+    if (req.session.user === 'student') next();
+    else res.redirect('/login.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
